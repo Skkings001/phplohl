d99cf0e6fa22972567edf9d56bafa26d46fe415e
Skkings001 <skpgamer001@outlook.com>
2024-10-01 01:03:47 +0530

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,3 +1,27 @@
 <?php
-phpinfo();
-var_dump(opcache_get_status());
+
+$url = 'https://babel-in.xyz/tata/channels';
+$data = [
+    'X-API-Key' => 'babel-40b84ca520bd0fe134cc01fc80',
+];
+$jsonData = json_encode($data);
+
+$options = [
+    'http' => [
+        'header'  => [
+            "Content-type: application/json",
+            "User-Agent: Babel/5.0"
+        ],
+        'method'  => 'POST',
+        'content' => $jsonData,
+    ],
+];
+
+$context  = stream_context_create($options);
+$response = file_get_contents($url, false, $context);
+
+if ($response === FALSE) {
+    die('Error sending request');
+}
+
+echo $response;
